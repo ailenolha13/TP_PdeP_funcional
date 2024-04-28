9b70061afad30ad7879d64f001e5ce18502cad4b
= <=>
2024-04-28 15:57:03 -0300

    Se modela ciudades, se crea funcion para obtener el valor de una ciudad, y se crea los tests

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,52 @@
 module Library where
 import PdePreludat
 
+-- Creamos el tipo ValorCiudad 
+type ValorCiudad = Number
+-- Creamos el tipo Anio 
+type Anio = Number
+
+-- Modelamos ciudades
+-- Nos interesa conocer su nombre, el año de fundación, las atracciones principales y su costo de vida
+data Ciudad = UnaCiudad {
+    nombre :: String,
+    anioFundacion :: Number,
+    atraccionesPrincipales :: [String],
+    costoVida :: Number
+} deriving (Show)
+
+-- Funcion para obtener el valor de una ciudad en base a 3 condiciones mencionadas en el enunciado del TP
+obtieneValor :: Ciudad -> ValorCiudad
+obtieneValor ciudad | fueFundadaAntesDe 1800 ciudad = es5VecesLaDiferencia 1800 (anioFundacion ciudad)
+                    | noTieneAtracciones ciudad = doble (costoVida ciudad)
+                    | otherwise = triple (costoVida ciudad)
+
+-- Funcion para saber si la ciudad fue fundanda antes del anio 1800
+fueFundadaAntesDe :: Anio -> Ciudad -> Bool
+fueFundadaAntesDe anio ciudad = anioFundacion ciudad < anio
+
+-- Funcion para calcular 5 veces la diferencia entre 1800 y el anio de fundacion
+es5VecesLaDiferencia :: Anio -> Anio -> Number
+es5VecesLaDiferencia anio anioFundacion = quintuple (resta anio anioFundacion)
+
+-- Funcion para saber si la ciudad tiene o no atracciones
+noTieneAtracciones :: Ciudad -> Bool
+noTieneAtracciones = null . atraccionesPrincipales
+
+-- Funciones utiles
 doble :: Number -> Number
 doble numero = numero + numero
+triple :: Number -> Number
+triple numero = 3 * numero
+quintuple :: Number -> Number
+quintuple numero = 5 * numero
+resta :: Number -> Number -> Number
+resta numero1 numero2 = numero1 - numero2
+
+-- Ciudades creadas solo con el proposito de poder probar las funciones rapidamente - DESPUES HAY QUE BORRARLAS:
+buenosAires:: Ciudad
+buenosAires = UnaCiudad {nombre = "Buenos Aires", anioFundacion = 1536, atraccionesPrincipales = ["Obelisco"], costoVida= 50}
+mendoza :: Ciudad
+mendoza = UnaCiudad {nombre = "Mendoza", anioFundacion = 1850, atraccionesPrincipales = [], costoVida= 20}
+sanLuis :: Ciudad
+sanLuis = UnaCiudad {nombre = "San Luis", anioFundacion = 1950, atraccionesPrincipales = ["Rapel", "Trekking", "Tirolesa"], costoVida= 100}
--- src/Spec.hs
+++ src/Spec.hs
@@ -5,7 +5,25 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
-
+  describe "Test de la funcion obtieneValor" $ do
+    it "185 * 5 = 925" $ do
+      obtieneValor UnaCiudad {
+        nombre = "Baradero"
+        , anioFundacion = 1615
+        , atraccionesPrincipales = ["Parque del Este", "Museo Alejandro Barbich"]
+        , costoVida= 150 } 
+        `shouldBe` 925
+    it "140 * 2 = 280" $ do
+      obtieneValor UnaCiudad {
+        nombre = "Nullish"
+        , anioFundacion = 1800
+        , atraccionesPrincipales = []
+        , costoVida= 140 } 
+        `shouldBe` 280
+    it "120 * 3 = 360" $ do
+      obtieneValor UnaCiudad {
+        nombre = "Caleta Olivia"
+        , anioFundacion = 1901
+        , atraccionesPrincipales = ["El Gorosito", "Faro Costanera"]
+        , costoVida= 120 } 
+        `shouldBe` 360
