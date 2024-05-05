76d9a69aa76e177f596afa72d965ce44ef6823f3
= <=>
2024-05-05 01:37:40 -0300

    test para la funcion agregarNuevaAtraccion

--- src/Spec.hs
+++ src/Spec.hs
@@ -8,46 +8,28 @@ correrTests = hspec $ do
   -- Test "Valor de una ciudad "
   describe "Test de la funcion obtieneValor" $ do
     it "El valor de la ciudad Baradero es 925" $ do
-      obtieneValor UnaCiudad {
-        nombre = "Baradero"
-        , anioFundacion = 1615
-        , atraccionesPrincipales = ["Parque del Este", "Museo Alejandro Barbich"]
-        , costoVida = 150 } 
+      obtieneValor baradero
         `shouldBe` 925
     it "El valor de la ciudad Nullish es 280" $ do
-      obtieneValor UnaCiudad {
-        nombre = "Nullish"
-        , anioFundacion = 1800
-        , atraccionesPrincipales = []
-        , costoVida = 140 } 
+      obtieneValor nullish
         `shouldBe` 280
     it "El valor de la ciudad Caleta Olivia es 360" $ do
-      obtieneValor UnaCiudad {
-        nombre = "Caleta Olivia"
-        , anioFundacion = 1901
-        , atraccionesPrincipales = ["El Gorosito", "Faro Costanera"]
-        , costoVida = 120 } 
+      obtieneValor caletaOlivia
         `shouldBe` 360
   -- Test "Alguna atraccion copada"
   describe "Test de la funcion tieneAtraccionCopada" $ do
     it "La ciudad Baradero no tiene atraccion copada" $ do
-      tieneAtraccionCopada UnaCiudad {
-        nombre = "Baradero"
-        , anioFundacion = 1615
-        , atraccionesPrincipales = ["Parque del Este", "Museo Alejandro Barbich"]
-        , costoVida = 150 } 
+      tieneAtraccionCopada baradero
         `shouldBe` False
     it "La ciudad Nullish no tiene atraccion copada" $ do
-      tieneAtraccionCopada UnaCiudad {
-        nombre = "Nullish"
-        , anioFundacion = 1800
-        , atraccionesPrincipales = []
-        , costoVida = 140 } 
+      tieneAtraccionCopada nullish
         `shouldBe` False
     it "La ciudad Caleta Olivia tiene atraccion copada" $ do
-      tieneAtraccionCopada UnaCiudad {
-        nombre = "Caleta Olivia"
-        , anioFundacion = 1901
-        , atraccionesPrincipales = ["El Gorosito", "Faro Costanera"]
-        , costoVida = 120 } 
+      tieneAtraccionCopada caletaOlivia
         `shouldBe` True
+  -- Test "Agregar una nueva atraccion"
+  describe "Test de la funcion agregarNuevaAtraccion" $ do
+    it "La ciudad Azul tiene 4 atracciones principales y su costo de vida es 228" $ do
+      agregarNuevaAtraccion "Balneario Municipal Alte. Guillermo Brown" azul
+      `shouldBe` azulNueva -- Creamos una instancia Eq en Library.hs para poder comparar 2 valores de tipo Ciudad en este test
+
